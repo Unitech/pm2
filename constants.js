554491e59e873b760999cbf929e8172463bcd0d2
Alexandre Strzelewicz <strzelewicz.alexandre@gmail.com>
2013-10-27 15:02:18 +0100

    #131 #105 - customize PM2 ports via env variables

--- constants.js
+++ constants.js
@@ -7,6 +7,8 @@ var p = require('path');
 
 DEFAULT_FILE_PATH = p.resolve(process.env.HOME, '.pm2');
 
+
+
 module.exports = {
   DEFAULT_FILE_PATH  : DEFAULT_FILE_PATH,
   PM2_LOG_FILE_PATH  : p.join(p.resolve(process.env.HOME, '.pm2'), 'pm2.log'),
@@ -15,13 +17,13 @@ module.exports = {
   DUMP_FILE_PATH     : p.join(DEFAULT_FILE_PATH, 'dump.pm2'),
 
   DAEMON_BIND_HOST   : process.env.PM2_BIND_ADDR || 'localhost',
-  DAEMON_RPC_PORT    : process.env.PM2_RPC_PORT  || 6666, // RPC commands
-  DAEMON_PUB_PORT    : process.env.PM2_PUB_PORT  || 6667, // Realtime events
-
+  DAEMON_RPC_PORT    : parseInt(process.env.PM2_RPC_PORT)  || 6666, // RPC commands
+  DAEMON_PUB_PORT    : parseInt(process.env.PM2_PUB_PORT)  || 6667, // Realtime events
+  
   CODE_UNCAUGHTEXCEPTION : 100,
   
   DEBUG              : process.env.PM2_DEBUG || false,
-  WEB_INTERFACE      : process.env.PM2_API_PORT  || 9615,
+  WEB_INTERFACE      : parseInt(process.env.PM2_API_PORT)  || 9615,
   MODIFY_REQUIRE     : false,
   PREFIX_MSG         : '\x1B[32mPM2 \x1B[39m',
   PREFIX_MSG_ERR     : '\x1B[31mPM2 [ERROR] \x1B[39m',
