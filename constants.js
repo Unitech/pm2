3584acfafd61078befc136fcb793619f734f941c
tknew2 <strzelewicz.alexandre@gmail.com>
2014-01-11 18:48:41 +0100

    interactor for pm2.io

--- constants.js
+++ constants.js
@@ -40,5 +40,8 @@ module.exports = {
   ONE_LAUNCH_STATUS  : 'one-launch-status',
 
   REMOTE_PORT        : 3900,
-  REMOTE_HOST        : 'socket-1.pm2.io'
+  REMOTE_HOST        : 'socket-1.pm2.io',
+  INTERACTOR_LOG_FILE_PATH : p.join(p.resolve(process.env.HOME, '.pm2'), 'interactor.log'),
+  INTERACTOR_PID_PATH : p.join(p.resolve(process.env.HOME, '.pm2'), 'interactor.pid'),
+  INTERACTOR_RPC_PORT : parseInt(process.env.PM2_INTERACTOR_PORT) || 6668
 };
--- examples/human_event.js
+++ examples/human_event.js
@@ -1,2 +1,4 @@
 
-process.send({type:"event:zlatan", msg:'hello im an human'});
+setInterval(function() {
+  process.send({type:"event:zlatan", msg:'hello im an human'});
+}, 1000);
