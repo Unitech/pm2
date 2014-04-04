236e37df0c7bfa7dde63db6f7513fa8b8b5db765
tknew2 <strzelewicz.alexandre@gmail.com>
2014-04-04 11:13:15 +0800

    Interactor update

--- examples/throw.js
+++ examples/throw.js
@@ -1,5 +1,5 @@
 
 setTimeout(function() {
   console.log('log message from echo auto kill');
-  throw new Error('exit with uncaught exception');
+  throw new Error('EXITED ALERT ROUGEEEEE');
 }, 2000);
