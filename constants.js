344cc9a819fad6597fdfda5199ab48146e754070
Michiel De Mey <de.mey.michiel@gmail.com>
2014-05-12 18:31:16 -0700

    Stripping environment variables from the HttpInterface

--- constants.js
+++ constants.js
@@ -33,6 +33,7 @@ var default_conf = {
 
   DEBUG              : process.env.PM2_DEBUG || false,
   WEB_INTERFACE      : parseInt(process.env.PM2_API_PORT)  || 9615,
+  WEB_STRIP_ENV_VARS : process.env.PM2_WEB_STRIP_ENV_VARS || true,
   MODIFY_REQUIRE     : process.env.PM2_MODIFY_REQUIRE || false,
   PREFIX_MSG         : '\x1B[32mPM2 \x1B[39m',
   PREFIX_MSG_ERR     : '\x1B[31mPM2 [ERROR] \x1B[39m',
