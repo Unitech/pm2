1cd2cf011037dbbe34241b962458da5f7e9f7c97
tknew2 <strzelewicz.alexandre@gmail.com>
2014-06-19 17:16:31 +0200

    add callback to Satan start function

---
+++ test/programmatic.js
@@ -0,0 +1,25 @@
+
+/**
+ * Test Satan in a programmatic way
+ */
+
+var Satan  = require('../lib/Satan.js');
+var should = require('should');
+var assert = require('better-assert');
+var path   = require('path');
+
+describe('Satan', function() {
+
+  after(function(done) {
+    Satan.killDaemon(done);
+  });
+
+  it('should start Satan interaction', function(done) {
+    Satan.start(function(err) {
+      should(err).be.null;
+      done();
+    });
+  });
+
+
+});
