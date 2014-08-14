d044b2c73de99387550a4b3316037975025c387e
tknew2 <strzelewicz.alexandre@gmail.com>
2014-08-14 11:48:33 +0200

    enable node-args and PM2_NODE_OPTIONS for Fork mode

--- test/bash/harmony.sh
+++ test/bash/harmony.sh
@@ -9,20 +9,35 @@ $pm2 kill
 echo "################ HARMONY ES6"
 
 $pm2 start harmony.js
-sleep 8
+sleep 2
 $pm2 list
-should 'should fail when trying to launch pm2 without harmony option' 'errored' 1
+ishould 'should not fail when passing harmony option to V8' 'restart_time: 0' 1
 $pm2 list
 $pm2 kill
 
 PM2_NODE_OPTIONS='--harmony' `pwd`/../../bin/pm2 start harmony.js
-sleep 4
+sleep 2
 $pm2 list
 should 'should not fail when passing harmony option to V8' 'restart_time: 0' 1
 $pm2 kill
 
 $pm2 start harmony.js --node-args="--harmony"
-sleep 8
+sleep 2
 $pm2 list
-should 'should not fail when passing node-args=harmony opts' 'errored' 0
+should 'should not fail when passing node-args=harmony opts' 'restart_time: 0' 1
+$pm2 kill
+
+echo "################ HARMONY / NODEARGS ES6 FORK MODE"
+
+$pm2 start harmony.js --node-args="--harmony" -x
+sleep 2
+$pm2 list
+should 'should not fail when passing node-args=harmony opts' 'restart_time: 0' 1
+$pm2 kill
+
+
+PM2_NODE_OPTIONS='--harmony' $pm2 start harmony.js -x
+sleep 2
+$pm2 list
+should 'should not fail when passing node-args=harmony opts' 'restart_time: 0' 1
 $pm2 kill
