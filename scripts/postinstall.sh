ff233a8204bdd1ef5b388109951ad715f7f1fc48
tknew2 <strzelewicz.alexandre@gmail.com>
2014-08-19 18:19:50 +0200

    preinstall permission

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ "$EUID" -eq 0 ]
-  then
-    echo "As you run PM2 as root, to update PM2 automatically you must add the --unsafe-perm flag else run the installation as non root user"
-  exit
-fi
-
 `command -v node || command -v nodejs` ./scripts/ping.js
 if [ $? -eq 0 ]
 then
--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-if [ "$EUID" -eq 0 ]
+#
+# Check if user is logged as root and that pm2 command is available
+#
+
+if ( [ "$EUID" -eq 0 ] || [ "$USER" == "root" ] ) && ! command -v pm2 2>&1;
   then
-    echo "As you run PM2 as root, to update PM2 automatically you must add the --unsafe-perm flag else run the installation as non root user"
-  exit
+    echo "##### PM2 INSTALLATION"
+    echo "#"
+    echo "#"
+    echo "# As you run PM2 as root, to update PM2 automatically"
+    echo "# you must add the --unsafe-perm flag."
+    echo "#"
+    echo "#       $  npm install pm2 -g --unsafe-perm"
+    echo "#"
+    echo "# Else run the installation as a non root user"
+    echo "#"
+    echo "#"
+    echo "#"
+    echo "######"
+    echo ""
+  exit 1
 fi
+
+which pm2
+
 
 `command -v node || command -v nodejs` ./scripts/ping.js
 if [ $? -eq 0 ]
