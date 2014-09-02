6995b61f396f568190753b0e108c7c44264e43b7
tknew2 <strzelewicz.alexandre@gmail.com>
2014-09-02 14:04:05 +0200

    #673

--- examples/child-env.js
+++ examples/child-env.js
@@ -1,6 +1,7 @@
+
 var http = require('http');
 
 http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('hey');
-}).listen(8000);
+  res.end(process.env.PORT_ENV);
+}).listen(process.env.PORT_ENV);
