81e65661507703b40a78fc4f000153ab4144dfcc
tknew2 <strzelewicz.alexandre@gmail.com>
2014-09-21 00:41:51 +0200

    update axm driver

--- test/programmatic/child.js
+++
@@ -1,7 +0,0 @@
-
-var http = require('http');
-
-http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('hoy');
-}).listen(8000);
--- test/programmatic/cwd.js
+++
@@ -1,7 +0,0 @@
-
-var http = require('http');
-
-http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end(__dirname);
-}).listen(8000);
