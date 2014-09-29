64413b016cbe2e2e23d82165f6ff9f71897fa22d
tknew2 <strzelewicz.alexandre@gmail.com>
2014-09-29 19:55:35 +0200

    update unix socket protocol

--- examples/keymetrics-load.js
+++ examples/keymetrics-load.js
@@ -9,12 +9,13 @@ pm2.connect(function() {
         pm2.start('examples/kill-not-so-fast.js',  {
           instances:10,
           minUptime: 0,
+          force:true,
           maxRestarts : 0
         },  function() {
           pm2.start('examples/auto-save.js', {execMode : 'fork', watch:true, force : true}, function() {
             pm2.start('examples/custom_action_with_params.js', function() {
               //pm2.start('examples/auto-bench.js', {instances : 'max'}, function() {
-              pm2.start('examples/throw.js', {name:'auto-throw'}, function() {
+              pm2.start('examples/throw.js', {force:true, name:'auto-throw'}, function() {
                 pm2.disconnect(function() { process.exit(1); });
               });
             });
