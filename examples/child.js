895ed0cc0b6c43b295ac528364da5eff3b37025a
tknew2 <strzelewicz.alexandre@gmail.com>
2014-10-10 14:47:35 +0200

    fix process.send exception

--- examples/child.js
+++ examples/child.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('hoy');
+  res.end('hey');
 }).listen(8000);
 
 process.on('message', function(msg) {
