f2997a0fb2cab5e1d81d23455dbb3eae910c250b
tknew2 <strzelewicz.alexandre@gmail.com>
2014-10-15 12:01:12 +0200

    configuration file + remove hashbang on top of bin/pm2 + remove custom_options.sh

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ var p    = require('path');
 var fs   = require('fs');
 var util = require('util');
 
-var HOME = process.env.PM2_HOME || process.env.HOME;
+var HOME = process.env.PM2_HOME || process.env.HOME || process.env.HOMEPATH;
 
 var DEFAULT_FILE_PATH = p.resolve(HOME, '.pm2');
 
@@ -11,75 +11,75 @@ var DEFAULT_FILE_PATH = p.resolve(HOME, '.pm2');
  * Constants variables used by PM2
  */
 var csts = {
-  SAMPLE_CONF_FILE   : p.join('..', 'lib', 'custom_options.sh'),
+  PM2_CONF_FILE : p.join(DEFAULT_FILE_PATH, 'conf.js'),
+
   CODE_UNCAUGHTEXCEPTION : 100,
-  CONCURRENT_ACTIONS : 1,
-  PREFIX_MSG         : '\x1B[32m[PM2] \x1B[39m',
-  PREFIX_MSG_ERR     : '\x1B[31m[PM2] [ERROR] \x1B[39m',
-  PREFIX_MSG_WARNING : '\x1B[33m[PM2] [WARN] \x1B[39m',
-  PREFIX_MSG_SUCCESS : '\x1B[36;1m[PM2] \x1B[39;0m',
-  SAMPLE_FILE_PATH   : '../lib/sample.json',
+  CONCURRENT_ACTIONS     : 1,
+  PREFIX_MSG             : '\x1B[32m[PM2] \x1B[39m',
+  PREFIX_MSG_ERR         : '\x1B[31m[PM2] [ERROR] \x1B[39m',
+  PREFIX_MSG_WARNING     : '\x1B[33m[PM2] [WARN] \x1B[39m',
+  PREFIX_MSG_SUCCESS     : '\x1B[36;1m[PM2] \x1B[39;0m',
 
-  CENTOS_STARTUP_SCRIPT : '../lib/scripts/pm2-init-centos.sh',
-  UBUNTU_STARTUP_SCRIPT : '../lib/scripts/pm2-init.sh',
-  SYSTEMD_STARTUP_SCRIPT: '../lib/scripts/pm2.service',
-  AMAZON_STARTUP_SCRIPT : '../lib/scripts/pm2-init-amazon.sh',
-  GENTOO_STARTUP_SCRIPT : '../lib/scripts/pm2',
+  SAMPLE_FILE_PATH       : '../lib/sample.json',
+  SAMPLE_CONF_FILE       : '../lib/sample-conf.js',
 
-  SUCCESS_EXIT       : 0,
-  ERROR_EXIT         : 1,
+  CENTOS_STARTUP_SCRIPT  : '../lib/scripts/pm2-init-centos.sh',
+  UBUNTU_STARTUP_SCRIPT  : '../lib/scripts/pm2-init.sh',
+  SYSTEMD_STARTUP_SCRIPT : '../lib/scripts/pm2.service',
+  AMAZON_STARTUP_SCRIPT  : '../lib/scripts/pm2-init-amazon.sh',
+  GENTOO_STARTUP_SCRIPT  : '../lib/scripts/pm2',
 
-  ONLINE_STATUS      : 'online',
-  STOPPED_STATUS     : 'stopped',
-  STOPPING_STATUS    : 'stopping',
-  LAUNCHING_STATUS   : 'launching',
-  ERRORED_STATUS     : 'errored',
-  ONE_LAUNCH_STATUS  : 'one-launch-status',
+  SUCCESS_EXIT           : 0,
+  ERROR_EXIT             : 1,
 
-  KEYMETRICS_ROOT_URL : 'root.keymetrics.io',
+  ONLINE_STATUS          : 'online',
+  STOPPED_STATUS         : 'stopped',
+  STOPPING_STATUS        : 'stopping',
+  LAUNCHING_STATUS       : 'launching',
+  ERRORED_STATUS         : 'errored',
+  ONE_LAUNCH_STATUS      : 'one-launch-status',
 
-  REMOTE_PORT         : 41624,
-  REMOTE_REVERSE_PORT : 43554,
-  REMOTE_HOST         : 's1.keymetrics.io',
-  INTERACTION_CONF    : p.join(DEFAULT_FILE_PATH, 'agent.json'),
-  SEND_INTERVAL       : 1000
+  KEYMETRICS_ROOT_URL    : 'root.keymetrics.io',
+
+  REMOTE_PORT            : 41624,
+  REMOTE_REVERSE_PORT    : 43554,
+  REMOTE_HOST            : 's1.keymetrics.io',
+  SEND_INTERVAL          : 1000
 };
 
 /**
  * Defaults variables
  */
 var default_conf = {
-  DEFAULT_FILE_PATH  : DEFAULT_FILE_PATH,
-  PM2_LOG_FILE_PATH  : p.join(DEFAULT_FILE_PATH, 'pm2.log'),
-  PM2_PID_FILE_PATH  : p.join(DEFAULT_FILE_PATH, 'pm2.pid'),
-  DEFAULT_PID_PATH   : p.join(DEFAULT_FILE_PATH, 'pids'),
-  DEFAULT_LOG_PATH   : p.join(DEFAULT_FILE_PATH, 'logs'),
-  DUMP_FILE_PATH     : p.join(DEFAULT_FILE_PATH, 'dump.pm2'),
+  DEFAULT_FILE_PATH        : DEFAULT_FILE_PATH,
+  PM2_LOG_FILE_PATH        : p.join(DEFAULT_FILE_PATH, 'pm2.log'),
+  PM2_PID_FILE_PATH        : p.join(DEFAULT_FILE_PATH, 'pm2.pid'),
+  DEFAULT_PID_PATH         : p.join(DEFAULT_FILE_PATH, 'pids'),
+  DEFAULT_LOG_PATH         : p.join(DEFAULT_FILE_PATH, 'logs'),
+  DUMP_FILE_PATH           : p.join(DEFAULT_FILE_PATH, 'dump.pm2'),
 
-  PM2_CONF_FILE      : p.join(DEFAULT_FILE_PATH, 'custom_options.sh'),
+  DAEMON_RPC_PORT          : p.join(DEFAULT_FILE_PATH, 'rpc.sock'),
+  DAEMON_PUB_PORT          : p.join(DEFAULT_FILE_PATH, 'pub.sock'),
+  INTERACTOR_RPC_PORT      : p.join(DEFAULT_FILE_PATH, 'interactor.sock'),
 
-  DAEMON_RPC_PORT    : p.join(DEFAULT_FILE_PATH, 'rpc.sock'),
-  DAEMON_PUB_PORT    : p.join(DEFAULT_FILE_PATH, 'pub.sock'),
-  INTERACTOR_RPC_PORT : p.join(DEFAULT_FILE_PATH, 'interactor.sock'),
+  GRACEFUL_TIMEOUT         : parseInt(process.env.PM2_GRACEFUL_TIMEOUT) || 8000,
 
-  GRACEFUL_TIMEOUT   : parseInt(process.env.PM2_GRACEFUL_TIMEOUT) || 8000,
-
-  DEBUG              : process.env.PM2_DEBUG || false,
-  WEB_INTERFACE      : parseInt(process.env.PM2_API_PORT)  || 9615,
-  MODIFY_REQUIRE     : process.env.PM2_MODIFY_REQUIRE || false,
+  DEBUG                    : process.env.PM2_DEBUG || false,
+  WEB_INTERFACE            : parseInt(process.env.PM2_API_PORT)  || 9615,
+  MODIFY_REQUIRE           : process.env.PM2_MODIFY_REQUIRE || false,
 
   INTERACTOR_LOG_FILE_PATH : p.join(DEFAULT_FILE_PATH, 'agent.log'),
-  INTERACTOR_PID_PATH : p.join(DEFAULT_FILE_PATH, 'agent.pid')
+  INTERACTOR_PID_PATH      : p.join(DEFAULT_FILE_PATH, 'agent.pid'),
+  INTERACTION_CONF         : p.join(DEFAULT_FILE_PATH, 'agent.json')
 };
 
 /**
  * Extend with optional configuration file
  */
-var OPTIONAL_CONFIGURATION_FILE = p.join(DEFAULT_FILE_PATH, 'conf.js');
 
-if (fs.existsSync(OPTIONAL_CONFIGURATION_FILE)) {
+if (fs.existsSync(csts.PM2_CONF_FILE)) {
   try {
-    var extra = require(OPTIONAL_CONFIGURATION_FILE);
+    var extra = require(csts.PM2_CONF_FILE)(DEFAULT_FILE_PATH);
     default_conf = util._extend(default_conf, extra);
   } catch(e) {
     console.error(e.stack || e);
