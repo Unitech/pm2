f0a1d45936cf7a3c969e4caba96546fd23255796
tknew2 <strzelewicz.alexandre@gmail.com>
2014-10-18 14:04:31 +0200

    optimization

--- examples/throw.js
+++ examples/throw.js
@@ -1,5 +1,5 @@
 
 setTimeout(function() {
   console.log('log message from echo auto kill');
-  throw new Error('Exitasdsadasdsda unacepted !!');
+  throw new Error('Exitasdsadasdsda unacepted 222222 !!');
 }, 2000);
