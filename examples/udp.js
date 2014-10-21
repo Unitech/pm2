b1a9e2710188252fae5ce02ac780191df432ed1b
tknew2 <strzelewicz.alexandre@gmail.com>
2014-10-21 15:25:24 +0200

    fix not tty

---
+++ examples/udp.js
@@ -0,0 +1,24 @@
+
+
+
+var punt = require('punt');
+var server = punt.bind('0.0.0.0:5000');
+var a = punt.connect('0.0.0.0:5000');
+var b = punt.connect('0.0.0.0:5000');
+var c = punt.connect('0.0.0.0:5000');
+
+server.on('message', function(msg){
+    console.log(msg);
+  });
+
+setInterval(function(){
+    a.send({ hello: 'world' });
+  }, 150);
+
+setInterval(function(){
+    b.send('hello world');
+  }, 150);
+
+setInterval(function(){
+    c.send(new Buffer('hello'));
+  }, 150);
