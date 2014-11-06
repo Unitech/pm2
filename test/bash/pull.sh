9f35ef70d11a37b32d765999fa9fed2c0f17fbd3
jshkurti <jshkurti@student.42.fr>
2014-11-06 17:59:07 +0100

    more tests for pull/backward/forward features

--- test/bash/pull.sh
+++ test/bash/pull.sh
@@ -10,28 +10,40 @@ cd $file_path
 #
 # Testing pull,forward,backward methods
 #
-git clone https://github.com/Unitech/angular-bridge.git
-$pm2 start ./angular-bridge/index.js --name angular
+git clone https://github.com/keymetrics/app-playground.git
+cd app-playground
+git checkout hotfix
+$pm2 start ./process.json --name app
+sleep 5
+
+OUT=`$pm2 ls | grep errored | wc -l`
+[ $OUT -eq 1 ] || fail "$1"
+success "$1"
 
 OUT=`$pm2 desc 0 | grep remote | egrep -oh 'https://([^ ]+)'`
-[ $OUT = "https://github.com/Unitech/angular-bridge.git" ] || fail "$1"
+[ $OUT = "https://github.com/keymetrics/app-playground.git" ] || fail "$1"
 success "$1"
 
-OUT=`$pm2 backward angular | wc -l`
+OUT=`$pm2 backward app | wc -l`
 [ $OUT -eq 13 ] || fail "$1"
 success "$1"
 
-OUT=`$pm2 forward angular | wc -l`
+OUT=`$pm2 forward app | wc -l`
 [ $OUT -eq 13 ] || fail "$1"
 success "$1"
 
-OUT=`$pm2 forward angular | wc -l`
+OUT=`$pm2 forward app | wc -l`
 [ $OUT -eq 2 ] || fail "$1"
 success "$1"
 
-OUT=`$pm2 pull angular | wc -l`
+OUT=`$pm2 pull app | wc -l`
 [ $OUT -eq 2 ] || fail "$1"
+success "$1"
+
+OUT=`$pm2 ls | grep "16 " | wc -l`
+[ $OUT -eq 1 ] || fail "$1"
 success "$1"
 
 $pm2 kill
-rm -rf ./angular-bridge
+cd ..
+rm -rf ./app-playground
