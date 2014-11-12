4b1dbc713da1ad90902105ecd3f75483d5b53bfc
tknew2 <strzelewicz.alexandre@gmail.com>
2014-11-12 18:02:26 +0100

    remove killInteract and deleteInteract - replaced now by interact [option]

--- test/bash/interact.sh
+++ test/bash/interact.sh
@@ -6,7 +6,7 @@ cd $file_path
 
 echo -e "\033[1mRunning Interaction tests:\033[0m"
 
-$pm2 killInteract
+$pm2 interact stop
 
 $pm2 interact
 
@@ -14,19 +14,19 @@ $pm2 interact XXX2 XXX3 homeloc
 
 $pm2 updatePM2
 
-$pm2 killInteract
+$pm2 interact stop
 
 $pm2 interact
 
-$pm2 infoInteract
+$pm2 interact info
 
-$pm2 infoInteract | grep "XXX2"
+$pm2 interact info | grep "XXX2"
 spec "Should have XXX2 has public key"
 
-$pm2 infoInteract | grep "XXX3"
+$pm2 interact info | grep "XXX3"
 spec "Should have XXX3 has public key"
 
 $pm2 list
 
-$pm2 killInteract
+$pm2 interact stop
 $pm2 kill
