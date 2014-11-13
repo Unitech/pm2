6b21bc9ab87af95317126e33d132e99e4d6f607a
Jean-Nicolas LAGNEAU <jlagneau@student.42.fr>
2014-11-13 10:24:15 +0100

    keep binary test DRY and simplify it

--- test/bash/binary.sh
+++ test/bash/binary.sh
@@ -7,33 +7,37 @@ echo -e "\033[1mRunning tests:\033[0m"
 
 cd $file_path
 
+function getInterpreter() {
+	echo `$pm2 prettylist | grep exec_interpreter | awk -F"'" '{print $2}'`
+}
+
 #
 # Testing pm2 execution of binary files
 #
 $pm2 start `which watch` -- ls
 
-OUT=`$pm2 prettylist | grep exec_interpreter | sed "s/[\'|,|:]//g" | awk -F' ' '{print $2}'`
+OUT=$(getInterpreter)
 [ $OUT = "none" ] || fail "$1"
 success "$1"
 
 $pm2 kill
 $pm2 start binary-js-file
 
-OUT=`$pm2 prettylist | grep exec_interpreter | sed "s/[\'|,|:]//g" | awk -F' ' '{print $2}'`
+OUT=$(getInterpreter)
 [ $OUT = "node" ] || fail "$1"
 success "$1"
 
 $pm2 kill
 $pm2 start binary-js-file.js
 
-OUT=`$pm2 prettylist | grep exec_interpreter | sed "s/[\'|,|:]//g" | awk -F' ' '{print $2}'`
+OUT=$(getInterpreter)
 [ $OUT = "node" ] || fail "$1"
 success "$1"
 
 $pm2 kill
 $pm2 start binary-py-file.py
 
-OUT=`$pm2 prettylist | grep exec_interpreter | sed "s/[\'|,|:]//g" | awk -F' ' '{print $2}'`
+OUT=$(getInterpreter)
 [ $OUT = "python" ] || fail "$1"
 success "$1"
 
