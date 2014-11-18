11443a3a953683b368c5fb861d2837ad10074e22
Unitech <as@unitech.io>
2014-11-18 11:16:35 +0100

    Add 2 probes to monitor PM2 and Agent

---
+++ examples/axm/pm2_probe.js
@@ -0,0 +1,24 @@
+var axm = require('axm');
+var fs  = require('fs');
+var path = require('path');
+
+fs.readFile(path.join(process.env.HOME, '.pm2', 'pm2.pid'), function(err, data) {
+
+  var pid = data.toString();
+
+  axm.configureModule({
+    name             : 'PM2',
+    version          : '0.12.1',
+    pid              : pid,
+    errors           : false,
+    latency          : false,
+    versioning       : false,
+    show_module_meta : true,
+    author           : 'Alexandre Strzelewicz',
+    comment          : 'This module monitors PM2'
+  });
+
+});
+
+setInterval(function() {
+}, 1000);
