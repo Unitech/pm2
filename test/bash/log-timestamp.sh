3b99e7a83c732db536b3a35073b97d95a8508642
Tjatse <thisnamemeansnothing@gmail.com>
2014-11-29 16:26:40 +0800

    fix: travis have no pm2.log but only stdout/stderr

--- test/bash/log-timestamp.sh
+++ test/bash/log-timestamp.sh
@@ -6,109 +6,94 @@ source "${SRC}/include.sh"
 function head {
   echo -e "\x1B[1;35m$1\x1B[0m"
 }
-function no_prefix {
-  sleep 0.3
-  OUT=`cat $pm2_log | grep -n "[0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}" | wc -l`
-  [ $OUT -eq 0 ] || fail "expect no timestamp prefix in pm2.log, but currently existing."
-  success "have no timestamp prefix"
+function rm_pm2log {
   if [ "$1" -ne 1 ]; then
     $pm2 kill
-    rm -rf $pm2_log
+    rm -rf ~/.pm2/pm2.log
   fi
 }
+function grep_log {
+  if [ -z $TRAVIS ]; then
+    echo "not travis"
+    eval "$pm2 $1"
+    sleep 0.3
+    OUT=`cat ~/.pm2/pm2.log | grep -n "[0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}" | wc -l`
+  else
+    echo "travis"
+    OUT=`eval "$pm2 $1 | grep -n \"[0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}\" | wc -l"`
+  fi
+}
+function no_prefix {
+  eval "grep_log \"$1\""
+  echo "line count: $OUT"
+  [ $OUT -eq 0 ] || fail "expect no timestamp prefix in pm2.log, but currently existing."
+  success "have no timestamp prefix"
+  rm_pm2log "$2"
+}
 function prefix {
-  sleep 0.3
-  OUT=`cat $pm2_log | grep -n "[0-9]\{4\}\-[0-9]\{2\}\-[0-9]\{2\}" | wc -l`
+  eval "grep_log \"$1\""
+  echo "line count: $OUT"
   [ $OUT -ne 0 ] || fail "expect have timestamp prefix in pm2.log, but currently does not exist."
   success "have timestamp prefix"
-  if [ "$1" -ne 1 ]; then
-    $pm2 kill
-    rm -rf $pm2_log
-  fi
+
+  rm_pm2log "$2"
 }
 
 cd $file_path
 
-if [ -z $PM2_HOME ]; then
-  if [ -z $HOME ];then
-    if [ -z $HOMEPATH ]; then
-      fail "can not locate pm2 home"
-    else
-      pm2_log="$HOMEPATH/.pm2"
-    fi
-  else
-    pm2_log="$HOME/.pm2"
-  fi
-else
-  pm2_log="$PM2_HOME"
-fi
-
-pm2_log="$pm2_log/pm2.log"
-
-echo "root: $pm2_log";
-
 $pm2 kill
 
-rm -rf $pm2_log
+if [ -z $TRAVIS ]
+then
+  echo "removing pm2.log"
+  rm -rf ~/.pm2/pm2.log
+fi
 
 unset PM2_LOG_DATE_FORMAT
 
 head ">> LIST (NO PREFIX)"
-$pm2 ls
-no_prefix 0
+no_prefix "ls" 0
 
 head ">> START (NO PREFIX)"
-$pm2 start echo.js
-no_prefix 1
+no_prefix "start echo.js" 1
 
 head ">> RESTART (NO PREFIX)"
-$pm2 restart echo
-no_prefix 1
+no_prefix "restart echo" 1
 
 head ">> STOP (NO PREFIX)"
-$pm2 stop echo
-no_prefix 0
+no_prefix "stop echo" 0
 
 head ">> START JSON (NO PREFIX)"
-$pm2 start echo-pm2.json
-no_prefix 1
+no_prefix "start echo-pm2.json" 1
 
 head ">> RESTART JSON (NO PREFIX)"
-$pm2 restart echo-pm2.json
-no_prefix 1
+no_prefix "restart echo-pm2.json" 1
 
 head ">> STOP-JSON (NO PREFIX)"
-$pm2 stop echo-pm2.json
-no_prefix 0
+no_prefix "stop echo-pm2.json" 0
 
 export PM2_LOG_DATE_FORMAT="YYYY-MM-DD HH:mm Z"
 
 head ">> LIST (PREFIX)"
-$pm2 ls
-prefix 0
+prefix "ls" 0
 
 head ">> START (PREFIX)"
-$pm2 start echo.js
-prefix 1
+prefix "start echo.js" 1
 
 head ">> RESTART (PREFIX)"
-$pm2 restart echo
-prefix 1
+prefix "restart echo" 1
 
 head ">> STOP (PREFIX)"
-$pm2 stop echo
-prefix 0
+prefix "stop echo" 0
 
 head ">> START JSON (PREFIX)"
-$pm2 start echo-pm2.json
-prefix 1
+prefix "start echo-pm2.json" 1
 
 head ">> RESTART JSON (PREFIX)"
-$pm2 restart echo-pm2.json
-prefix 1
+prefix "restart echo-pm2.json" 1
 
 head ">> STOP-JSON (PREFIX)"
-$pm2 restart echo-pm2.json
-prefix 0
+prefix "restart echo-pm2.json" 0
 
 unset PM2_LOG_DATE_FORMAT
+touch ~/.pm2/pm2.log
