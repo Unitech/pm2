a5f1a63d0d2ebd78c27d8091eced85a301ad837d
Unitech <as@unitech.io>
2015-03-01 10:42:22 -0500

    auto init pm2 when using it programmatically

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./lib/CLI.js');
+
+var CLI = require('./lib/CLI.js');
+
+/**
+ * Ensure that PM2 has been inited when using it programmatically
+ */
+CLI.pm2Init();
+
+module.exports = CLI;
