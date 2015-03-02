b26714db651fadd5e725819ad952dc1fa1163474
Unitech <as@unitech.io>
2015-03-01 19:56:53 -0500

    fixtest - configuration file path

--- test/bash/get_set.sh
+++ test/bash/get_set.sh
@@ -8,13 +8,13 @@ cd $file_path
 $pm2 unset all
 spec "Should unset all variables"
 
-ls ~/.pm2/module_env.json
+ls ~/.pm2/module_conf.json
 spec "Should file exists"
 
 $pm2 set key1 val1
-cat ~/.pm2/module_env.json | grep "key1"
+cat ~/.pm2/module_conf.json | grep "key1"
 spec "Should key exists"
 
 $pm2 unset key1
-cat ~/.pm2/module_env.json | grep "key1"
+cat ~/.pm2/module_conf.json | grep "key1"
 ispec "Should key not exists"
