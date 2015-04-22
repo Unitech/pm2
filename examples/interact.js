1af64a132564d7e0ee7d85228867935edafd7aa4
--global <alexandre@keymetrics.io>
2015-04-21 22:01:22 -0400

    add interact example

--- examples/interact.js
+++ examples/interact.js
@@ -1,13 +1,13 @@
 
-var pm2 = require('./.');
+var pm2 = require('..');
 
 var MACHINE_NAME = 'hk1';
 var PRIVATE_KEY  = 'z1ormi95vomgq66';
 var PUBLIC_KEY   = 'oa0m7nuhdfibi16';
 
-pm2.connect(function() {
+pm2.connect(true, function() {
   pm2.start({
-    script : 'test/fixtures/child.js',
+    script : '../test/fixtures/child.js',
     name : 'production-app'
   }, function() {
     pm2.interact(PRIVATE_KEY, PUBLIC_KEY, MACHINE_NAME, function() {
