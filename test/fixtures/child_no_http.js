493d9016c4636c9db8e400a38c6f7de813d499ba
Joni Shkurti <jshkurti@student.42.fr>
2015-04-23 20:15:25 +0200

    added tests for pmx injection in fork and cluster mode

---
+++ test/fixtures/child_no_http.js
@@ -0,0 +1,20 @@
+var pmx  = require('pmx').init({
+  http: false
+});
+
+var http = require('http');
+
+http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('hey');
+}).listen(8000);
+
+process.on('message', function(msg) {
+  if (msg == 'shutdown') {
+    console.log('Closing all connections...');
+    setTimeout(function() {
+      console.log('Finished closing connections');
+      process.exit(0);
+    }, 100);
+  }
+});
