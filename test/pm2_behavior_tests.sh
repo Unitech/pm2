1981ab049d3ac6ae6641d4abb4dca454337311e8
jshkurti <jshkurti@student.42.fr>
2015-04-29 19:25:59 +0200

    wrapped fork_mode + various test

--- test/pm2_behavior_tests.sh
+++ test/pm2_behavior_tests.sh
@@ -67,6 +67,8 @@ bash ./test/bash/module.sh
 spec "module system"
 bash ./test/bash/vizion.sh
 spec "vizion features (versioning control)"
+bash ./test/bash/pmx_injection.sh
+spec "automatic pmx injection"
 
 bash ./test/bash/inside-pm2.sh
 spec "Starting a process inside a PM2 process"
