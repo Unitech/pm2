47f801b4b0201336056939c6677c03bf838d0742
Unitech <strzelewicz.alexandre@gmail.com>
2015-06-10 12:52:48 +0200

    #1333 smart json restart via pm2 start <json_file> + pm2 [reload|gracefulReload] <json_file>

--- test/bash/json_file.sh
+++ test/bash/json_file.sh
@@ -22,6 +22,29 @@ $pm2 restart all.json
 should 'should stop processes' 'online' 6
 should 'should all script been restarted one time' 'restart_time: 1' 6
 
+$pm2 reload all.json
+sleep 1
+should 'should reload processes' 'online' 6
+should 'should all script been restarted one time' 'restart_time: 2' 6
+
+$pm2 gracefulReload all.json
+sleep 1
+should 'should graceful reload processes' 'online' 6
+should 'should all script been restarted one time' 'restart_time: 3' 6
+
+##
+## Smart restart
+##
+$pm2 start all.json
+should 'should smart restart processes' 'online' 6
+should 'should all script been restarted one time' 'restart_time: 4' 6
+
+$pm2 stop all.json
+should 'should smart restart processes' 'stopped' 6
+
+$pm2 start all.json
+should 'should smart restart processes' 'online' 6
+
 $pm2 kill
 
 ########## JS style
