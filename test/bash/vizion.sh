00618261c902f5a44af7ec2f013cb4b6e4f071c9
jshkurti <jshkurti@student.42.fr>
2015-06-16 16:47:47 +0200

    fix tests

--- test/bash/vizion.sh
+++ test/bash/vizion.sh
@@ -14,18 +14,18 @@ $pm2 kill
 $pm2 start echo.js
 sleep 1
 
-should 'should have versioning metadata' 'github.com' 1
+should 'should have versioning metadata' 'jshkurti/pm2_travis' 1
 
 $pm2 delete all
 $pm2 start echo.js --no-vizion
 sleep 1
 
-should 'should not have versioning metadata' 'github.com' 0
+should 'should not have versioning metadata' 'jshkurti/pm2_travis' 0
 
 $pm2 delete all
 $pm2 start no-vizion.json
 sleep 1
 
-should 'should not have versioning metadata' 'github.com' 0
+should 'should not have versioning metadata' 'jshkurti/pm2_travis' 0
 
 mv .git git
