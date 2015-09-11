6e3bf4d38ba94353898cf5d509b151aa6f0dd7ad
Unitech <strzelewicz.alexandre@gmail.com>
2015-09-11 14:20:50 +0200

    add module test

--- test/pm2_programmatic_tests.sh
+++ test/pm2_programmatic_tests.sh
@@ -42,6 +42,8 @@ mocha ./test/programmatic/logs.js
 spec "Logs test"
 mocha ./test/programmatic/watcher.js
 spec "Watcher"
+mocha ./test/programmatic/modularizer.mocha.js
+spec "Module system"
 mocha ./test/programmatic/max_memory_limit.js
 spec "Max memory tests"
 mocha ./test/programmatic/cluster.js
---
+++ test/programmatic/modularizer.mocha.js
@@ -0,0 +1,55 @@
+
+var pm2         = require('../..');
+var should      = require('should');
+var Modularizer = require('../../lib/Modularizer');
+
+describe('Modularizer programmatic tests', function() {
+
+  before(function(done) {
+    process.env.PM2_WORKER_INTERVAL = 1000;
+
+    pm2.connect(function() {
+      pm2.kill(function() {
+        pm2.connect(function() {
+          done();
+        });
+      });
+    });
+  });
+
+  it('should have methods', function() {
+    Modularizer.should.have.property('getAdditionalConf');
+  });
+
+  it('should uninstall modules', function(done) {
+    Modularizer.uninstall('all', done);
+  });
+
+  it('should list 0 module', function() {
+    var ret = Modularizer.listModules();
+    ret.length.should.eql(0);
+  });
+
+  it('should install a module', function(done) {
+    Modularizer.install('.', done);
+  });
+
+  it('should list 1 module', function(done) {
+    pm2.list(function(err, procs) {
+      procs.length.should.eql(1);
+      done()
+    });
+  });
+
+  it('should uninstall a module', function(done) {
+    Modularizer.uninstall('pm2', done);
+  });
+
+  it('should list 0 module', function(done) {
+    pm2.list(function(err, procs) {
+      procs.length.should.eql(0);
+      done()
+    });
+  });
+
+});
