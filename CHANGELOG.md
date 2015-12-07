256da4822c994c4c7d8c1fc09d5a5e4333df2a76
Unitech <strzelewicz.alexandre@gmail.com>
2015-12-07 16:23:26 +0100

    Fix cluster kill signal + avoid writing on std output when proc disconnected #1819

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,8 @@
 
 ### 0.15.11
 
+- Send SIGINT signal to process instead of SIGTERM [#1819]
+- Avoid writing on std err/out when process is disconnected [#1819]
 - Upgrade pm2-axon, cron, should, mocha, coffee-script, chokidar, semver NPM packages
 - Show process configuration option when describing process
 - Add --no-automation flag
--- examples/beforeExit.js
+++ examples/beforeExit.js
@@ -1,25 +1,21 @@
-'use strict';
 
-let stopped = false;
+var stopped = false;
 
 function work() {
-    console.log('working');
-    !stopped && setTimeout(work, 200);
-  }
-
-function stop() {
-  stopped = true;
-  console.log('shutting down', stopped);
+  console.log('working');
+  !stopped && setTimeout(work, 1000);
 }
 
-process.once('SIGINT', function() {
-  console.log('SIGINT');
-  stop();
-});   // CTRL-C
-process.once('SIGTERM', function() {
-  console.log('SIGTERM');
-  stop();
-});  // pm2 stop
-process.on('beforeExit', () => console.log('exited cleanly :)'));
+function stop() {
+  console.log('shutting down');
+  stopped = true;
+}
+
+process.once('SIGINT', stop);   // CTRL-C
+
+process.on('beforeExit', function() {
+  console.log('exited cleanly :)');
+  process.exit(0);
+});
 
 work();
