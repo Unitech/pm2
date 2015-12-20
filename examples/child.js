7935fa6042325d10bef7c724536b0fdcfde6fe8c
Unitech <strzelewicz.alexandre@gmail.com>
2015-12-20 22:57:29 +0100

    minor: update examples

--- examples/child.js
+++ examples/child.js
@@ -1,10 +1,14 @@
-require('pmx').init({
+var pmx = require('pmx').init({
   http : true
 });
+
+var probe = pmx.probe();
 
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('hey');
-}).listen(8000);
+}).listen(8000, function() {
+  console.log('App listening on port 8000');
+});
