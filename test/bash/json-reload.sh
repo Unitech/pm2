9f7dff816168f9209ebd6bf4dc7071e2d81f9f11
Unitech <strzelewicz.alexandre@gmail.com>
2016-03-15 17:52:32 +0100

    refactor: in progress

--- test/bash/json-reload.sh
+++ test/bash/json-reload.sh
@@ -8,6 +8,8 @@ cd $file_path
 
 echo -e "\033[1mRunning tests:\033[0m"
 
+cd json-reload
+
 #
 # Max memory auto restart option
 #
@@ -16,28 +18,29 @@ echo -e "\033[1mRunning tests:\033[0m"
 $pm2 kill
 
 PM2_WORKER_INTERVAL=1000 $pm2 start max-mem-0.json
-sleep 3
+sleep 2
 $pm2 list
 should 'process should has not been restarted' 'restart_time: 0' 1
 
 $pm2 restart max-mem.json
 
-sleep 3
+sleep 2
 $pm2 list
-should 'process should has been restarted' 'restart_time: 1' 0
+should 'process should has been restarted' 'restart_time: 0' 0
 
+#
+# Date format change
+#
 $pm2 delete all
 
 CURRENT_YEAR=`date +"%Y"`
-
 >echo-test.log
 
 $pm2 start echo-pre.json
-sleep 2
-
+sleep 1
 grep $CURRENT_YEAR echo-test.log
 spec "Should have written year in log file according to format YYYY"
-grep "SUSU" echo-test.log
+grep "ok" echo-test.log
 spec "Should have written new string depending on ECHO_MSG"
 
 $pm2 restart echo-post.json
@@ -45,7 +48,7 @@ $pm2 restart echo-post.json
 sleep 1
 
 grep $CURRENT_YEAR echo-test.log
-ispec "Should have not written year in log file according to format"
+ispec "Should have written year in log file according to format"
 
 grep "YAY" echo-test.log
 spec "Should have written new string depending on ECHO_MSG"
@@ -53,14 +56,22 @@ spec "Should have written new string depending on ECHO_MSG"
 # Switch to production environment
 $pm2 restart echo-post.json --env production
 >echo-test.log
-
 sleep 1
-
 grep "WOW" echo-test.log
 spec "Should have written new string depending on ECHO_MSG"
 
-# Go back to original environment
+#
+# Switch to production environment
+#
+$pm2 reload echo-post.json --env production
+>echo-test.log
+sleep 1
+grep "WOW" echo-test.log
+spec "Should have written new string depending on ECHO_MSG"
 
+#
+# Go back to original environment
+#
 $pm2 restart echo-post.json
 sleep 1
 grep "YAY" echo-test.log
