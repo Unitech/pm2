ab5499fc693c1aa912d2578328da527fde1be73e
Unitech <strzelewicz.alexandre@gmail.com>
2016-04-01 19:35:05 +0200

    refactor push connection

---
+++ examples/client.js
@@ -0,0 +1,73 @@
+
+
+var axon = require('pm2-axon');
+
+var server = axon.socket('sub');
+
+server.bind(8080);
+
+server.on('bind', function() {
+  console.log('Server ready');
+});
+
+server.on('message', function(data) {
+  console.log(data);
+});
+
+// setTimeout(function() {
+//   console.log('Closing server');
+//   server.close(function() {
+//     console.log('Closed');
+//   });
+// }, 3000);
+
+function setupConnection() {
+  var client = axon.socket('pub');
+
+  client.on('connect', function() {
+    console.log('Client connected');
+  });
+
+  client.on('error', function(e) {
+    console.log('Client got error', e.message);
+  });
+
+  client.on('close', function(e) {
+    console.log('Client got a close');
+  });
+
+  client.on('reconnect attempt', function(e) {
+    console.log('Reconnecting');
+  });
+
+  client.connect(8080);
+
+  this.send = function() {
+    client.send({success:true});
+  };
+
+  this.destroy = function() {
+    client.close();
+    client.removeAllListeners();
+  };
+
+  this.reconnect = function() {
+
+  };
+  return this;
+}
+
+var connection = setupConnection();
+
+connection.on('close', function() {
+  console.log('AYA');
+});
+
+setInterval(function() {
+  connection.send();
+}, 1000);
+
+setInterval(function() {
+  connection.destroy();
+  connection = setupConnection();
+}, 2000);
