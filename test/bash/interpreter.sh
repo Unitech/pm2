117eec1062e7d38eca77b934b6265e0b37082729
Unitech <strzelewicz.alexandre@gmail.com>
2016-07-24 11:33:12 -0700

    now transpilators are optionnal and installable via pm2 install

--- test/bash/interpreter.sh
+++ test/bash/interpreter.sh
@@ -5,6 +5,26 @@ source "${SRC}/include.sh"
 
 cd $file_path/interpreter
 
+########### coffee
+
+$pm2 start echo.coffee
+sleep 1
+should 'process should be errored without coffee installed' "status: 'errored'" 1
+
+########### Install
+
+$pm2 install coffeescript
+
+########### coffee fork test
+$pm2 delete all
+
+$pm2 start echo.coffee
+
+sleep 1.5
+
+should 'process should not have been restarted' 'restart_time: 0' 1
+should 'process should be online' "status: 'online'" 1
+
 ########### coffee cluster test
 $pm2 delete all
 
@@ -15,16 +35,17 @@ sleep 1.5
 should 'process should not have been restarted' 'restart_time: 0' 1
 should 'process should be online' "status: 'online'" 1
 
-########### livescript cluster test
+
+########## LIVESCRIPT
+
 $pm2 delete all
+$pm2 start echo.ls
+sleep 1
+should 'process should be errored without coffee installed' "status: 'errored'" 1
 
->livescript.log
+########### Install
 
-$pm2 start echo.ls -i 1 -o livescript.log --merge-logs
-
-sleep 1.5
-grep "Hello Livescript!" livescript.log
-spec "Should work on Livescript files in cluster mode"
+$pm2 install livescript
 
 ########### livescript fork test
 $pm2 delete all
@@ -37,16 +58,27 @@ sleep 1.5
 grep "Hello Livescript!" livescript.log
 spec "Should work on Livescript files in fork mode"
 
-########### typescript cluster test
+########### livescript cluster test
 $pm2 delete all
 
->typescript.log
+>livescript.log
 
-$pm2 start echo.ts -i 1 -o typescript.log --merge-logs
+$pm2 start echo.ls -i 1 -o livescript.log --merge-logs
 
 sleep 1.5
-grep "Hello Typescript!" typescript.log
-spec "Should work on Typescript files in cluster mode"
+grep "Hello Livescript!" livescript.log
+spec "Should work on Livescript files in cluster mode"
+
+########### TYPESCRIPT
+
+$pm2 delete all
+$pm2 start echo.ts
+sleep 1
+should 'process should be errored without coffee installed' "status: 'errored'" 1
+
+########### Install
+
+$pm2 install typescript
 
 ########### typescript fork test
 $pm2 delete all
@@ -59,3 +91,14 @@ sleep 1.5
 
 grep "Hello Typescript!" typescript.log
 spec "Should work on Typescript files in fork mode"
+
+########### typescript cluster test
+$pm2 delete all
+
+>typescript.log
+
+$pm2 start echo.ts -i 1 -o typescript.log --merge-logs
+
+sleep 1.5
+grep "Hello Typescript!" typescript.log
+spec "Should work on Typescript files in cluster mode"
