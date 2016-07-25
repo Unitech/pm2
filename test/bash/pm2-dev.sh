207f664739195a335bd61b3b1176deef609eeb3a
Unitech <strzelewicz.alexandre@gmail.com>
2016-07-25 09:15:30 -0700

    pmd err check + test fix

--- test/bash/pm2-dev.sh
+++ test/bash/pm2-dev.sh
@@ -4,11 +4,9 @@ SRC=$(cd $(dirname "$0"); pwd)
 source "${SRC}/include.sh"
 
 pm2dev="`type -P node` `pwd`/bin/pm2-dev"
-pmd="`type -P node` `pwd`/bin/pm2-dev"
+pmd="`type -P node` `pwd`/bin/pmd"
 
-$pm2 link delete
-
-$pmd child.js &
+$pmd test/fixtures/child.js &
 PM2_PID=$!
 
 sleep 2
