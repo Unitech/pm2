77dc344e130dd41aa6c81e2225b6ad9edb060980
Unitech <strzelewicz.alexandre@gmail.com>
2016-07-31 16:26:06 -0700

    rundev fix test

--- test/bash/pm2-dev.sh
+++ test/bash/pm2-dev.sh
@@ -4,9 +4,9 @@ SRC=$(cd $(dirname "$0"); pwd)
 source "${SRC}/include.sh"
 
 pm2dev="`type -P node` `pwd`/bin/pm2-dev"
-pmd="`type -P node` `pwd`/bin/pmd"
+rundev="`type -P node` `pwd`/bin/rundev"
 
-$pmd test/fixtures/child.js &
+$rundev test/fixtures/child.js &
 PM2_PID=$!
 
 sleep 2
