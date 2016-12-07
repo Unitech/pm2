7a018c53f71bf8a5d01e4a308b26e645b84c6fe7
Unitech <strzelewicz.alexandre@gmail.com>
2016-12-07 22:00:18 +0100

    #1090 #15 do not replicate same processes on resurrect

--- test/pm2_behavior_tests.sh
+++ test/pm2_behavior_tests.sh
@@ -23,6 +23,8 @@ bash ./test/bash/pm2-dev.sh
 spec "pm2-dev"
 bash ./test/bash/startup.sh
 spec "upstart startup test"
+bash ./test/bash/resurrect.sh
+spec "resurrect test"
 # bash ./test/bash/docker.sh
 # spec "Docker tests"
 bash ./test/bash/nvm-node-version.sh
