b2c2e4bddc1fad28690ca3026f507320fbbe5bb9
Unitech <strzelewicz.alexandre@gmail.com>
2016-12-09 17:56:57 +0100

    (deb) move files all over the place and fix description

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 gen_deb:
-	./scripts/build-dist.sh
-	./scripts/build-deb.sh
+	./packager/build-dist.sh
+	./packager/build-deb.sh
 
 clean:
 	find node_modules \( -name "example" -o -name "examples" -o -name "docs" -o -name "jsdoc" -o -name "jsdocs" -o -name "test" -o -name "tests" -o -name "*\.md" -o -name "*\.html" -o -name "*\.eot" -o -name "*\.svg" -o -name "*\.woff" \) -print -exec rm -rf {} \;
--- packager/build-deb.sh
+++ packager/build-deb.sh
@@ -43,7 +43,7 @@ mv $PACKAGE_TMPDIR/dist/index.js $PACKAGE_TMPDIR/usr/share/pm2/
 #mv $PACKAGE_TMPDIR/dist/lib-legacy $PACKAGE_TMPDIR/usr/share/pm2/
 mv $PACKAGE_TMPDIR/dist/node_modules $PACKAGE_TMPDIR/usr/share/pm2/
 mv $PACKAGE_TMPDIR/dist/package.json $PACKAGE_TMPDIR/usr/share/pm2/
-cp resources/debian/copyright $PACKAGE_TMPDIR/usr/share/doc/pm2/copyright
+cp packager/debian/copyright $PACKAGE_TMPDIR/usr/share/doc/pm2/copyright
 
 # These are unneeded and throw lintian lint errors
 rm -f $PACKAGE_TMPDIR/usr/share/pm2/node_modules/node-uuid/benchmark/bench.gnu
@@ -61,7 +61,7 @@ ln -s ../share/pm2/bin/pm2 $PACKAGE_TMPDIR/usr/bin/pm2
 # Common FPM parameters for all packages we'll build using FPM
 FPM="fpm --input-type dir --chdir $PACKAGE_TMPDIR --name pm2 --version $VERSION "`
   `"--vendor 'Keymetrics <contact@keymetrics.io>' --maintainer 'Alexandre Strzelewicz <alexandre@keymetrics.io>' "`
-  `"--url https://pm2.io/ --license AGPLv3 --description '$(cat resources/debian/description)'"
+  `"--url https://pm2.io/ --license AGPLv3 --description '$(cat packager/debian/description)'"
 
 ##### Build RPM (CentOS, Fedora) package
 eval "$FPM --output-type rpm  --architecture noarch --depends nodejs --category 'Development/Languages' ."
@@ -70,14 +70,14 @@ mv *.rpm $OUTPUT_DIR
 ##### Build DEB (Debian, Ubuntu) package
 mkdir -p $PACKAGE_TMPDIR/DEBIAN
 mkdir -p $PACKAGE_TMPDIR/usr/share/lintian/overrides/
-cp resources/debian/lintian-overrides $PACKAGE_TMPDIR/usr/share/lintian/overrides/pm2
+cp packager/debian/lintian-overrides $PACKAGE_TMPDIR/usr/share/lintian/overrides/pm2
 
 # Debian/Ubuntu call the Node.js binary "nodejs", not "node".
 sed -i 's/env node/env nodejs/' $PACKAGE_TMPDIR/usr/share/pm2/bin/pm2
 
 # Replace variables in Debian package control file
 INSTALLED_SIZE=`du -sk $PACKAGE_TMPDIR | cut -f 1`
-sed -e "s/\$VERSION/$VERSION/;s/\$INSTALLED_SIZE/$INSTALLED_SIZE/" < resources/debian/control.in > $PACKAGE_TMPDIR/DEBIAN/control
+sed -e "s/\$VERSION/$VERSION/;s/\$INSTALLED_SIZE/$INSTALLED_SIZE/" < packager/debian/control.in > $PACKAGE_TMPDIR/DEBIAN/control
 fakeroot dpkg-deb -b $PACKAGE_TMPDIR $DEB_PACKAGE_NAME
 mv $DEB_PACKAGE_NAME $OUTPUT_DIR
 
