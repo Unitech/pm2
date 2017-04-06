8460660231fafa4492cb559b2ca411da062e8084
Unitech <strzelewicz.alexandre@gmail.com>
2017-04-06 22:56:05 +0200

    #2818 pm2-docker -> pm2-runtime + tests

--- test/bash/pm2-runtime.sh
+++ test/bash/pm2-runtime.sh
@@ -3,22 +3,22 @@
 SRC=$(cd $(dirname "$0"); pwd)
 source "${SRC}/include.sh"
 
-pm2dev="`type -P node` `pwd`/bin/pm2-dev"
+pm2runtime="`type -P node` `pwd`/bin/pm2-runtime"
 
-export PM2_HOME=$HOME'/.pm2-dev'
+export PM2_RUNTIME_DEBUG='true'
 
 cd $file_path/pm2-dev
 
+$pm2 delete all
+
 # Test with js
-$pm2dev app.js --test-mode
-$pm2 ls
-should 'should have started 1 apps' 'online' 1
-should 'should watch be true' 'watch: true' 1
-$pm2 kill
+$pm2runtime start app.js -i 4
+should 'should have started 4 apps' 'online' 4
+
+$pm2 delete all
 
 # Test with json and args
-$pm2dev start app.json --test-mode
-$pm2 ls
+$pm2runtime start app.json
 should 'should have started 1 apps' 'online' 1
 $pm2 prettylist | grep "watch: \[ 'server', 'client' \]"
 spec "Should application have two watch arguments"
--- test/pm2_behavior_tests.sh
+++ test/pm2_behavior_tests.sh
@@ -21,6 +21,8 @@ spec "Second hard cli tests"
 # Power feature
 bash ./test/bash/pm2-dev.sh
 spec "pm2-dev"
+bash ./test/bash/pm2-runtime.sh
+spec "pm2-runtime"
 bash ./test/bash/startup.sh
 spec "upstart startup test"
 bash ./test/bash/resurrect.sh
