694f5473bdbaea784770555f4342d842a301a759
vmarchaud <contact@vmarchaud.fr>
2017-04-19 16:57:20 +0200

    (tests) add unit test for getCanonicModuleName method

---
+++ test/interface/utility.mocha.js
@@ -0,0 +1,25 @@
+
+var assert = require('assert');
+var Utility = require('../../lib/Utility.js');
+
+describe('Utility', function() {
+  describe('.getCanonicModuleName', () => {
+    it('should get null without invalid parameters', function() {
+      assert(Utility.getCanonicModuleName() === null);
+      assert(Utility.getCanonicModuleName(/aa/) === null);
+      assert(Utility.getCanonicModuleName(111) === null);
+      assert(Utility.getCanonicModuleName({}) === null);
+    });
+
+    it('should works with all notation', function() {
+      assert(Utility.getCanonicModuleName('ma-zal/pm2-slack') === 'pm2-slack');
+      assert(Utility.getCanonicModuleName('pm2-slack@1.0.0') === 'pm2-slack');
+      assert(Utility.getCanonicModuleName('pm2-slack-1.0.0.tgz') === 'pm2-slack');
+      assert(Utility.getCanonicModuleName('ma-zal/pm2-slack') === 'pm2-slack');
+      assert(Utility.getCanonicModuleName('ma-zal/pm2-slack#own-branch') === 'pm2-slack');
+      assert(Utility.getCanonicModuleName('pm2-slack') === 'pm2-slack');
+      assert(Utility.getCanonicModuleName('@org/pm2-slack') === 'pm2-slack');
+    });
+  });
+  
+});
