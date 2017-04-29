09b7c5e1723a399a7630f351a289d0b4281d23f8
Unitech <strzelewicz.alexandre@gmail.com>
2017-04-29 02:08:46 +0200

    pm2 monitor|umonitor <pm_id|app_name|all>

--- test/pm2_behavior_tests.sh
+++ test/pm2_behavior_tests.sh
@@ -119,6 +119,8 @@ bash ./test/bash/wait-ready-event.sh
 spec "Wait for application ready event"
 bash ./test/bash/serve.sh
 spec "pm2 serve CLI method"
+bash ./test/bash/monit.sh
+spec "km selective monitoring "
 bash ./test/bash/log-null.sh
 spec "Logging path set to null"
 bash ./test/bash/log-json.sh
