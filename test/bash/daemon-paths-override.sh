22df02093e3f848d354e390806dc41e65f51f3c3
vmarchaud <contact@vmarchaud.fr>
2017-05-21 11:58:38 +0200

    (tests) fix failing tests

--- test/bash/daemon-paths-override.sh
+++ test/bash/daemon-paths-override.sh
@@ -4,31 +4,12 @@ SRC=$(cd $(dirname "$0"); pwd)
 source "${SRC}/include.sh"
 
 $pm2 kill
-
-########### Override PM2 log path
-PM2_LOG_FILE_PATH=/tmp/.toto.log $pm2 ls
-
-sleep 2
-test -f /tmp/.toto.log 
-
-spec 'should have picked the pm2 log path'
-
-$pm2 kill
-rm /tmp/.toto.log
+rm /tmp/.toto.pid
 
 ########### Override PM2 pid path
 PM2_PID_FILE_PATH=/tmp/.toto.pid $pm2 ls
 
 sleep 2
-! test -f /tmp/.toto.log 
 test -f /tmp/.toto.pid 
 
 spec 'should have picked the pm2 pid path'
-
-$pm2 kill
-
-sleep 2
-! test -f /tmp/.toto.pid 
-! test -f /tmp/.toto.log
-
-spec 'should have deleted pm2 pid file in custom path'
