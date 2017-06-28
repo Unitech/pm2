0cb60c81364060b49ee17e08ae1cec54893ee5b4
Unitech <strzelewicz.alexandre@gmail.com>
2017-06-28 17:43:10 +0200

    #2968 poc

---
+++ examples/stdin.js
@@ -0,0 +1,17 @@
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.prompt();
+
+rl.on('line', (data) =>{
+  console.log('Received data');
+  console.log(data);
+});
+
+setInterval(() => {
+}, 100);
