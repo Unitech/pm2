829d95dd2f084cdb3bf47f1142db1fecec030eab
bisubus <subus.gh@gmail.com>
2017-07-06 00:00:22 +0300

    Add resurrection tests for a backup

--- test/bash/resurrect.sh
+++ test/bash/resurrect.sh
@@ -6,13 +6,23 @@ cd $file_path
 
 $pm2 start echo.js -i 4
 spec "should start 4 processes"
+should 'should have 4 apps started' 'online' 4
+
 $pm2 save
-spec "should save process list"
-ls ~/.pm2/dump.pm2
-spec "should dump file exists"
 $pm2 resurrect
-spec "should resurrect"
+spec "should resurrect from dump"
 should 'should have still 4 apps started' 'online' 4
+
+$pm2 save
+$pm2 delete all
+echo "[{" > ~/.pm2/dump.pm2
+$pm2 resurrect
+spec "should resurrect from backup if dump is broken"
+ls ~/.pm2/dump.pm2
+ispec "should delete broken dump"
+should 'should have still 4 apps started' 'online' 4
+
 $pm2 delete all
 $pm2 resurrect
+spec "should resurrect from backup if dump is missing"
 should 'should have still 4 apps started' 'online' 4
