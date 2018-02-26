282186f24b19b010999f7c7c49750935ef19c190
Unitech <strzelewicz.alexandre@gmail.com>
2018-02-26 20:19:22 +0100

    refactor: change params

--- test/docker_parallel_test.sh
+++ test/docker_parallel_test.sh
@@ -3,9 +3,9 @@ set -e
 docker build -t pm2-test -f test/Dockerfile .
 
 JOBS=20
-
+OPTS="--jobs $JOBS --no-notice --joblog joblog-X docker run -v `pwd`:/var/pm2 pm2-test"
 # cli ok
-ls test/bash/cli/* | parallel --jobs $JOBS --no-notice --halt now,success=0 --joblog joblog-X docker run -v `pwd`:/var/pm2 pm2-test bash
+ls test/bash/cli/* | parallel $OPTS bash
 # process-file ok
 ls test/bash/process-file/* | parallel --jobs $JOBS --no-notice --halt now,success=0 --joblog joblog-X docker run -v `pwd`:/var/pm2 pm2-test bash
 # logs ok
