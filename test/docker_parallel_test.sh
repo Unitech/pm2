938027117cdb2f300ee772ab27f008cbe22a4b19
Unitech <strzelewicz.alexandre@gmail.com>
2018-02-26 20:46:11 +0100

    refactor e2e one line parallel

--- test/docker_parallel_test.sh
+++ test/docker_parallel_test.sh
@@ -5,19 +5,4 @@ docker build -t pm2-test -f test/Dockerfile .
 JOBS=20
 OPTS="--jobs $JOBS --joblog joblog-X docker run -v `pwd`:/var/pm2 pm2-test"
 
-# process-file ok
-ls test/e2e/process-file/* | parallel $OPTS bash
-# cli ok
-ls test/e2e/cli/* | parallel $OPTS bash
-# logs ok
-ls test/e2e/logs/* | parallel $OPTS bash
-# reload ok
-ls test/e2e/reload/* | parallel $OPTS bash
-# internals ok
-ls test/e2e/internal/* | parallel $OPTS bash
-# modules ok
-ls test/e2e/modules/* | parallel $OPTS bash
-# binaries ok
-ls test/e2e/binaries/* | parallel $OPTS bash
-
-# misc
+ls test/e2e/binaries/* test/e2e/modules/* test/e2e/internal/* test/e2e/reload/* test/e2e/process-file/* test/e2e/cli/* test/e2e/logs/* | parallel $OPTS bash
