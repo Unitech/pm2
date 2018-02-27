e7c67dddf52e7aab2ab616795fc6aa4eeac1585b
Unitech <strzelewicz.alexandre@gmail.com>
2018-02-27 01:10:51 +0100

    refactor: name tests well

--- test/docker_parallel_test.sh
+++ test/docker_parallel_test.sh
@@ -5,4 +5,6 @@ docker build -t pm2-test -f test/Dockerfile .
 JOBS=20
 OPTS="--jobs $JOBS --joblog joblog-X docker run -v `pwd`:/var/pm2 pm2-test"
 
-ls test/e2e/binaries/* test/e2e/modules/* test/e2e/internal/* test/e2e/reload/* test/e2e/process-file/* test/e2e/cli/* test/e2e/logs/* | parallel $OPTS bash
+ls test/e2e/cli/* | parallel $OPTS bash
+
+#ls test/e2e/binaries/* test/e2e/modules/* test/e2e/internal/* test/e2e/process-file/* test/e2e/cli/* test/e2e/logs/* | parallel $OPTS bash
