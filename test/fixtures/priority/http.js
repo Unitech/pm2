069fcf74101324bfdce56f3d9cde7d0d2e712bae
Livii Iabanzhi <livannnn@yandex.ru>
2018-08-03 14:34:48 +0300

    Added tests for start/restart priority

---
+++ test/fixtures/priority/http.js
@@ -0,0 +1,10 @@
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('hey');
+}).listen(process.env.PORT || 8000, function() {
+  console.log('App listening on port %d', server.address().port);
+  process.send('pm2:started');
+});
