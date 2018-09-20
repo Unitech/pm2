b4708e7498e1b63a9b3cf7dd5c5414cfb2906ddb
Unitech <alexandre@keymetrics.io>
2018-09-20 14:10:37 +0200

    chore: adapt tests

--- test/fixtures/ecosystem.config.js
+++ test/fixtures/ecosystem.config.js
@@ -1,33 +1,22 @@
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
-  apps : [
+  apps : [{
+    name: 'API',
+    script: 'app.js',
 
-    // First application
-    {
-      name      : 'API',
-      script    : 'app.js',
-      env: {
-        COMMON_VARIABLE: 'true'
-      },
-      env_production : {
-        NODE_ENV: 'production'
-      }
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
     },
-
-    // Second application
-    {
-      name      : 'WEB',
-      script    : 'web.js'
+    env_production: {
+      NODE_ENV: 'production'
     }
-  ],
+  }],
 
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
   deploy : {
     production : {
       user : 'node',
@@ -36,17 +25,6 @@ module.exports = {
       repo : 'git@github.com:repo.git',
       path : '/var/www/production',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    },
-    dev : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
     }
   }
 };
