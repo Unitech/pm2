0f1c2c6a246c5033c83ac37eb261775377e2226e
Unitech <alexandre@keymetrics.io>
2018-09-23 23:33:42 +0200

    faster logs throughput date parsing by replacing moment by date-fns in PM2 Daemon + homogeneize all logs with same date format + factorize ForkMode logging

--- test/fixtures/stdout-stderr.js
+++ test/fixtures/stdout-stderr.js
@@ -6,3 +6,13 @@ process.stdout.write('outwrite', 'utf8', function() {
 process.stderr.write('errwrite', 'utf8', function() {
   console.log('errcb');
 });
+
+setInterval(function() {
+  process.stdout.write('outwrite', 'utf8', function() {
+    console.log('outcb');
+  });
+
+  process.stderr.write('errwrite', 'utf8', function() {
+    console.log('errcb');
+  });
+}, 1000)
