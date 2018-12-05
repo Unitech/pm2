eccd37da3fb04e14d691ef3d6f96f5df05d2f00f
Alexandre Strzelewicz <strzelewicz.alexandre@gmail.com>
2018-12-05 11:38:55 +0100

    test:fix port release test bin path

--- test/e2e/misc/port-release.sh
+++ test/e2e/misc/port-release.sh
@@ -6,8 +6,8 @@ cd $file_path
 
 echo -e "\033[1mRunning tests:\033[0m"
 
-/usr/local/bin/pm2 start cluster-pm2.json
+$pm2 start cluster-pm2.json
 should 'should have started 4 processes' 'online' 4
 
-/usr/local/bin/pm2 reload cluster-pm2.json
+$pm2 reload cluster-pm2.json
 should 'should have started 4 processes' 'online' 4
