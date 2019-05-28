f47c648b7f075bd90667a829f21781a23d1048b8
Unitech <alexandre@keymetrics.io>
2019-05-29 00:33:11 +0200

    feat: allow to auto dump on start/stop/delete via pm2 set pm2:autodump true #2078 #3431

--- test/programmatic/resurect_state.mocha.js
+++ test/programmatic/resurect_state.mocha.js
@@ -5,63 +5,109 @@ var path   = require('path');
 var fs     = require('fs');
 
 var cst = require('../../constants.js');
+var Configuration = require('../../lib/Configuration.js');
 
 describe('Keep state on pm2 update', function() {
-  var pm2 = new PM2.custom({
-    cwd : __dirname + '/../fixtures'
-  });
+  var pm2
 
-  after(function(done) {
-    pm2.kill(done);
-  });
+  before((done) => {
+    Configuration.set('pm2:autodump', 'true', function(err, data) {
+        pm2 = new PM2.custom({
+          cwd : __dirname + '/../fixtures'
+        });
 
-  before(function(done) {
-    pm2.connect(function() {
-      pm2.delete('all', function() {
+      should.not.exists(err);
+      done();
+    });
+  })
+
+  after((done) => {
+    Configuration.set('pm2:autodump', 'false', function(err, data) {
+      should.not.exists(err);
+      done();
+    });
+  })
+
+  describe('Should autosave edits on stop/start/delete', function() {
+
+    after(function(done) {
+      pm2.kill(done);
+    });
+
+    before(function(done) {
+      pm2.connect(function() {
+        pm2.delete('all', function() {
+          done();
+        });
+      });
+    });
+
+    it('should set autodump to true', function(done) {
+      pm2.set('pm2:autodump', 'true', function(err, data) {
+        should.not.exists(err);
+        done();
+      });
+    })
+
+    it('should start 4 processes', function(done) {
+      pm2.start({
+        script    : './echo.js',
+        instances : 4,
+        name      : 'echo'
+      }, function(err, data) {
+        should(err).be.null();
         done();
       });
     });
-  });
 
-  it('should start 4 processes', function(done) {
-    pm2.start({
-      script    : './echo.js',
-      instances : 4,
-      name      : 'echo'
-    }, function(err, data) {
-      should(err).be.null();
-      done();
-    });
-  });
+    it('should kill pm2', function(done) {
+      pm2.kill(done)
+    })
 
-  it('should stop 1 process', function(done) {
-    pm2.stop(0, done)
-  })
-
-  it('should dump', function(done) {
-    pm2.dump(done)
-  })
-
-  it('should kill pm2', (done) => {
-    pm2.kill(done)
-  })
-
-  it('should resurect with one process stopped', function(done) {
-    var started = 0, stopped = 0
-
-    pm2.resurrect(() => {
-      pm2.list((err, dt) => {
-        dt.forEach(proc => {
-          if (proc.pm2_env.status == 'stopped')
-            stopped++
-          else if (proc.pm2_env.status == 'online')
-            started++
+    it('should resurect with one process stopped', function(done) {
+      pm2.resurrect(() => {
+        pm2.list((err, dt) => {
+          if (dt.length == 4)
+            return done()
+          return done(new Error('Did not kept process status'))
         })
-        if (started == 3 && stopped == 1)
-          return done()
-        return done(new Error('Did not kept process status'))
+      })
+    })
+
+    it('should stop 1 process', function(done) {
+      pm2.stop(0, done)
+    })
+
+    it('should kill pm2', function(done) {
+      pm2.kill(done)
+    })
+
+    it('should resurect with one process stopped', function(done) {
+      pm2.resurrect(() => {
+        pm2.list((err, dt) => {
+          if (dt.length == 4 && dt.filter(proc => proc.pm2_env.status == 'stopped').length == 1)
+            return done()
+          return done(new Error('Did not kept process status'))
+        })
+      })
+    })
+
+    it('should delete and save', function(done) {
+      pm2.delete(0, done)
+    })
+
+    it('should kill pm2', function(done) {
+      pm2.kill(done)
+    })
+
+    it('should resurect with one process stopped', function(done) {
+      pm2.resurrect(() => {
+        pm2.list((err, dt) => {
+          if (dt.length == 3)
+            return done()
+          return done(new Error('Did not kept process status'))
+        })
       })
     })
   })
-
 })
