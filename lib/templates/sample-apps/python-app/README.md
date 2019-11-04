23ef47607ee50cb42c16c9cf2c58fee189b1aed8
Unitech <alexandre@keymetrics.io>
2019-11-04 10:07:51 +0100

    feature: pm2 create command

---
+++ lib/templates/sample-apps/python-app/README.md
@@ -0,0 +1,4 @@
+
+# Managing a Python Application
+
+On this boilerlate you will see how you can start a Python application with PM2.
---
+++ lib/templates/sample-apps/python-app/echo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import time
+
+while 1:
+    print("Start : %s" % time.ctime())
+    print("second line")
+    time.sleep(1)
