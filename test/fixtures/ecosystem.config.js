d5d7f7b7868d1f40ca0aede2675ebee333fe15b4
Unitech <alexandre@keymetrics.io>
2020-04-13 00:29:21 +0200

    bump lodash

--- test/fixtures/ecosystem.config.js
+++ test/fixtures/ecosystem.config.js
@@ -1,28 +1,18 @@
 module.exports = {
   apps : [{
-    name: 'API',
-    script: 'app.js',
-
-    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
+    script: 'index.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
   }],
 
   deploy : {
     production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'GIT_BRANCH',
+      repo : 'GIT_REPOSITORY',
       path : '/var/www/production',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
     }
