42a10f4abbda88eab68695ec363f7b43ae1af632
Unitech <alexandre@keymetrics.io>
2020-04-23 15:42:47 +0200

    bump @pm2/js-api + mocharc.yml -> mocharc.js

---
+++ .mocharc.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  'allow-uncaught' : false,
+  'async-only': false,
+  bail: true,
+  color: true,
+  delay: false,
+  diff: true,
+  exit: true,
+  timeout: 10000,
+  'trace-warnings': true,
+  ui: 'bdd',
+  retries: 2
+}
