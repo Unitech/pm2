7e049d5a9a33d108fa53b37ff62b56ec1431f866
Daniel <daniel.reus@olisto.com>
2023-11-01 16:10:13 +0100

    stdin/stdout/stderr object -> Stream

--- test/fixtures/post_start_hook/post_start_hook_normal.js
+++ test/fixtures/post_start_hook/post_start_hook_normal.js
@@ -4,9 +4,9 @@
  * This is a post-start hook that will be called after an app started.
  * @param {object} info
  * @param {string} info.pid The apps PID
- * @param {object} info.stdin The apps STDIN stream
- * @param {object} info.stdout The apps STDOUT stream
- * @param {object} info.stderr The apps STDERR stream
+ * @param {Stream} info.stdin The apps STDIN stream
+ * @param {Stream} info.stdout The apps STDOUT stream
+ * @param {Stream} info.stderr The apps STDERR stream
  * @param {object} pm2_env The apps environment variables
  * @returns {void}
  */
