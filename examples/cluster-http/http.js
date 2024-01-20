b455ac95b38e93502a7755a5bd21538023b7afbd
Your Name <you@example.com>
2024-01-20 13:10:26 +0100

    update package-lock.json deps

--- examples/cluster-http/http.js
+++ examples/cluster-http/http.js
@@ -5,5 +5,5 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('hey');
 }).listen(process.env.PORT || 8089, '0.0.0.0', function() {
-  console.log('App listening on port %d', server.address().port);
+  console.log('App listening on port 8089');
 });
